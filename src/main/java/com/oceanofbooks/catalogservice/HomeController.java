6fe3f4d0c787ab608794384688cc7c298cec560a
Raj Shivaram <raj.shivaram@hotmail.com>
2023-03-26 20:02:43 +0530

    Added multiple files
    Added new files and updated few files with config beans

--- src/main/java/com/oceanofbooks/catalogservice/HomeController.java
+++ src/main/java/com/oceanofbooks/catalogservice/HomeController.java
@@ -3,12 +3,21 @@ package com.oceanofbooks.catalogservice;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.oceanofbooks.catalogservice.config.OceanProperties;
+
 @RestController
 public class HomeController {
 
+    private final OceanProperties oceanProperties;
+
+    public HomeController(OceanProperties oceanProperties) {
+
+        this.oceanProperties = oceanProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return oceanProperties.getGreeting();
     }
 
 }
