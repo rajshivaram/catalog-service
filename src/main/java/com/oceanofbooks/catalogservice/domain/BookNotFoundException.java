1c80aac254e577a1e88879cf7f966f23baedfa6b
Raj Shivaram <raj.shivaram@hotmail.com>
2023-03-29 19:34:48 +0530

    Changed Code to Maven
    Changed Code to Maven

---
+++ src/main/java/com/oceanofbooks/catalogservice/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.oceanofbooks.catalogservice.domain;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
